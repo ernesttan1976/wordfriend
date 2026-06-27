86a306ef8c56931640752a770420c5bcf7285369
Ernest Tan <ernesttan1976@gmail.com>
2026-06-27 09:21:42 +0800

    feat: add missing import for shared_preferences in QuizScreen

--- app/lib/screens/quiz_screen.dart
+++ app/lib/screens/quiz_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter_tts/flutter_tts.dart';
 import 'package:just_audio/just_audio.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../api_client.dart';
 import '../models.dart';
